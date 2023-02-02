a84056a90be1edcfe6744c1c3d3fec50071e04af
Bhushan Srivastava <59949692+he11owthere@users.noreply.github.com>
2023-02-02 20:28:07 +0530

    Set Matrix Zeroes
    First commit.
    Problem 1

---
+++ Set_matrix_zeroes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+#include<unordered_set>
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> rows, col;
+        for(int i = 0; i < matrix.size(); i++)  
+        {
+            for(int j = 0; j < matrix[i].size(); j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    rows.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        for(auto it: rows)
+        {
+            for(int i = 0; i < matrix[0].size(); i++)
+                matrix[it][i] = 0;
+        }
+        cout<<"\n";
+        for(auto it: col)
+        {
+            for(int i = 0; i < matrix.size(); i++)
+                matrix[i][it] = 0;
+        }
+
+    }
+
+};
